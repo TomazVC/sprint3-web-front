29af029642ff9dea67490b2d7add2b8da7447564
Rony Ken <ronyknagai@gmail.com>
2023-09-14 19:50:47 -0300

    Form home feito, falta parte de validação, identar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Cabecalho from "../components/Cabecalho";
 import Rodape from "../components/Rodape";
+import Mainhome from '../components/Mainhome'
 import "../style/pages/main.scss";
 
 export default function Home(){
     return(
         <>
             <Cabecalho/>
+            <Mainhome/>
             <Rodape/>
         </>
     )
