d6a6f297659d381cbb2022ec534be4f47063277d
PedroPereiradosSantos37 <rm552047@fiap.com.br>
2023-09-15 19:03:31 -0300

    Criação da página produto

---
+++ src/components/Asideproduto.jsx
@@ -0,0 +1,24 @@
+import Exemplotank from "../img/exemplotank.png";
+import Reproducao from  "../img/reproducao.png";
+import Exemplohomem from "../img/exemplo_homem.webp";
+import Exemplo2tank from "../img/exemplo2tank.png";
+import "../style/components/asideproduto.scss";
+
+export default function asideproduto(){
+    const ExemplotankAlt = "Exemplotank";
+    const ReproducaoAlt = "Reproducao";
+    const ExemplohomemAlt = "Exemplohomem";
+    const Exemplo2tankAlt = "Exemplo2tank";
+
+    return(
+        <>
+            <aside>
+                <img src={Exemplotank} alt={ExemplotankAlt}/>
+                <img src={Reproducao} alt={ReproducaoAlt}/>
+                <img src={Exemplohomem} alt={ExemplohomemAlt}/>
+                <img src={Exemplo2tank} alt={Exemplo2tankAlt}/>
+            </aside>
+        </>
+    )
+}
+
---
+++ src/components/Mainproduto.jsx
@@ -0,0 +1,35 @@
+
+export default function Mainproduto(){
+
+    return (
+        <>
+        <main>
+            <div id="faixa-matriz"></div>
+                <div id="div-inicial">
+                    <div>
+                        <h2>Como o Aquatank funciona?</h2>
+                        <p>
+                        A resposta proposta pela aquatank está na vida marinha, mais
+                        especificamente nas algas e sua capacidade de filtrar o ar. Em
+                        parceria com grandes empresas que possuem compromisso
+                        socioambiental, buscamos criar a maior floresta liquida do mundo!
+                        </p>
+                        <p>
+                        Em aquários de água salgada, espalhados pela cidade, há criações de
+                        fitoplanctons e spirulinas.Cada 1 metro quadrado de aquatank produz
+                        a mesma quantidade de oxigênio que 10 metros quadrados de árvores,
+                        criando assim micro bolsões verdes, que purificam c02.
+                        </p>
+                    </div>
+                    
+                </div>
+                <div id="imagens">
+                    
+
+                    
+                </div>
+        </main>
+
+        </>
+    )
+}
