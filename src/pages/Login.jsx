260130ac2bc1fbce3077de512de0007784084ff3
tomaz versolato carballo <RM551417@fiap.com.br>
2023-09-15 18:00:36 -0300

    Login estilização feito.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,11 @@
 import Cabecalho from "../components/Cabecalho";
 import Rodape from "../components/Rodape";
 import Mainlogin from "../components/Mainlogin";
+import "../style/pages/Login.scss";
 
 export default function Login(){
     return(
-        <div class="container">
+        <div class="container-Login">
             <Cabecalho/>
             <Mainlogin/>
             <Rodape/>
