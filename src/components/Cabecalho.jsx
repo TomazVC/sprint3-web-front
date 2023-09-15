443af51144819fbf7028a9795db1ba4e7fe24685
tomaz versolato carballo <RM551417@fiap.com.br>
2023-09-15 16:55:42 -0300

    Troca de cor nos li.

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,24 +1,45 @@
-import "../style/components/cabecalho.scss";
-import {Link} from 'react-router-dom'
+import React, {useState, useEffect} from "react";
+import {Link, useLocation} from "react-router-dom";
 import AquaLogo from "../img/aquatank_logo.png";
+import "../style/components/cabecalho.scss";
 
 export default function Cabecalho(){
-    let AquaLogoAlt = "AquaLogo";
+    const AquaLogoAlt = "AquaLogo";
+    const location = useLocation();
+    const [linkAtivo, setLinkAtivo] = useState("");
+
+    useEffect(() => {
+    if(location.pathname === "/Produto"){
+        setLinkAtivo("Produto");
+    }
+    else if(location.pathname === "/Contratar"){
+        setLinkAtivo("Contratar");
+    }
+    else if(location.pathname === "/PeloMundo"){
+        setLinkAtivo("PeloMundo");
+    }
+    else{
+        setLinkAtivo("");
+    }
+    },[location.pathname]);
+
+    const estiloLink = (link) => ({
+        color: link === linkAtivo ? "#6BF8D9" : "#5e9186",
+    });
+
     return(
         <>
-        <header>
-                <Link to={'/'}>
-                <img src={AquaLogo} alt={AquaLogoAlt}/>
-                </Link>
-            <nav>
-                <ul>        
-                    <li><Link to={'/Produto'}>Produto</Link></li>
-                    <li><Link to={'/Contratar'}>Como Contratar</Link></li>
-                    <li><Link to={'/PeloMundo'}>AquaT pelo mundo</Link></li>
-                    <li><Link to={'/Login'} class='laranjinha'>Login</Link></li>
-                </ul>
-            </nav>
-        </header>
+            <header>
+                <Link to={"/"}><img src={AquaLogo} alt={AquaLogoAlt} /></Link>
+                <nav>
+                    <ul>
+                        <li><Link to={"/Produto"} style={estiloLink("Produto")}>Produto</Link></li>
+                        <li><Link to={"/Contratar"} style={estiloLink("Contratar")}>Como Contratar</Link></li>    
+                        <li><Link to={"/PeloMundo"} style={estiloLink("PeloMundo")}>AquaT pelo mundo</Link></li>    
+                        <li><Link to={"/Login"} className="laranjinha">Login</Link></li>
+                    </ul>
+                </nav>
+            </header>
         </>
-    )
+    );
 }
--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -1,19 +1,45 @@
-import "../style/components/rodape.scss";
+import React, {useState, useEffect} from "react";
 import SoftForge from "../img/SF_IBM.png";
-import {Link} from 'react-router-dom'
+import {Link, useLocation} from "react-router-dom";
+import "../style/components/rodape.scss";
 
 export default function Rodape(){
-    let SoftForgeAlt = "SoftForgeAlt"
+    const SoftForgeAlt = "SoftForgeAlt"
+    const location = useLocation();
+    const [linkAtivo, setLinkAtivo] = useState("");
+
+    useEffect(() => {
+        if(location.pathname === "/TrabalheAqui"){
+            setLinkAtivo("TrabalheAqui");
+        }
+        else if(location.pathname === "/Construcao"){
+            setLinkAtivo("Construcao");
+        }
+        else if(location.pathname === "/Construcao"){
+            setLinkAtivo("Construcao2");
+        }
+        else if(location.pathname === "/Construcao"){
+            setLinkAtivo("Construcao3");
+        }
+        else{
+            setLinkAtivo("");
+        }
+        },[location.pathname]);
+    
+        const estiloLink = (link) => ({
+            color: link === linkAtivo ? "#6BF8D9" : "#5e9186",
+        });
+
     return(
         <>
             <footer>
                 <img src={SoftForge} alt={SoftForgeAlt} class="logo2"/>
                 <nav>
                     <ul>
-                        <li><Link to={'/TrabalheAqui'}>Trabalhe aqui</Link></li>
-                        <li><Link to={'/Construcao'}>Contato</Link></li>
-                        <li><Link to={'/Construcao'}>Para Empresas</Link></li>
-                        <li><Link to={'/Construcao'}>Blog</Link></li>
+                        <li><Link to={'/TrabalheAqui'} style={estiloLink("TrabalheAqui")}>Trabalhe aqui</Link></li>
+                        <li><Link to={'/Construcao'} style={estiloLink("Construcao")}>Contato</Link></li>
+                        <li><Link to={'/Construcao'} style={estiloLink("Construcao2")}>Para Empresas</Link></li>
+                        <li><Link to={'/Construcao'} style={estiloLink("Construcao3")}>Blog</Link></li>
                     </ul>
                 </nav>
                 <div>
