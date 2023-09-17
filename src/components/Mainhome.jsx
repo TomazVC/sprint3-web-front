08277183b013a7b93e7d4dda50acc74375f21e33
Lilith Gouveia <lilithgouveia@outlook.com>
2023-09-17 16:54:01 -0300

    Adicionando lógica jsx da página home e movendo alguns arquivos.

--- src/components/Mainhome.jsx
+++ src/components/Mainhome.jsx
@@ -1,31 +1,177 @@
-import "../style/components/mainhome.scss";
+import { useState } from 'react';
+import InputMask from 'react-input-mask';
+import '../style/components/mainhome.scss';
 
-export default function Mainhome(){
-    return(
-        <>
-        <main>
-            <div>
-                <h2 id="title">Cansado de respirar ar sujo?</h2>
-                <p className="initial-text">Asma, infecções das vias aéreas superiores e incidência de câncer de pulmão e doenças cardiovasculares são apenas alguns dos sintomas que respirar poluição causa ao ser humano.
-                </p>
-                <p className="initial-text">
-                Junte-se a iniciativa capaz de acabar com isso!
-                </p>
-                <form name="increva-se">
-                <input type="text" placeholder="Alexandre Carlos " required="required" name="nome" />
-                <p></p>
-                <input type="tel" placeholder="(xx)xxxxx-xxxx" pattern="\(\d{2}\)\s*\d{5}-\d{4}" maxLength="15"/>
-                <p></p>
-                <input type="text" placeholder="nome@mail.com" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"/>
-                <p></p>
-                <textarea placeholder="Deixe suas dúvidas ou comentários"></textarea>
-                <button>Enviar</button>
-                </form>
-                <button id="btn-compartilhar">Compartilhe nas redes sociais!</button>
-            </div>
-        </main>
-        </>
-    )
-}
+export default function Mainhome() {
+  // Usando estados
+  const [name, setName] = useState(''); //guardando nome
+  const [email, setEmail] = useState(''); //guardando email
+  const [tel, setTel] = useState(''); //guardando telefone
 
-<label><input type="text" /></label>
+  // Função que escuta quando o click acontece, dispara as validações e em caso de validação correta, dispara um alerta de cadastro realizado.
+
+  function handleClick(e) {
+    e.preventDefault();
+    if (validation() === true) {
+      alert('Cadastro realizado!');
+    }
+  }
+
+  //  Validando formulário
+  function validation() {
+    let re = /\S+@\S+\.\S+/;
+    if (name.length < 2) {
+      alert('Dados inválidos, nome precisa ter no máximo 2 caracteres');
+      return false;
+    }
+    if (tel.length < 11) {
+      alert('Dados inválidos, formato: (11)11111-1111');
+      return false;
+    }
+    if (re.test(email) !== true) {
+      alert('Dados inválidos, formato: nome@mail.com');
+      return false;
+    }
+    return true;
+  }
+
+  //   Compartilhando nas redes sociais
+
+  function handleShare() {
+    // URL que deseja compartilhar
+    var textoCompartilhar =
+      'Asma, infecções das vias aéreas superiores e incidência de câncer de pulmão e doenças cardiovasculares são apenas alguns dos sintomas que respirar poluição causa ao ser humano. Junte-se a iniciativa capaz de acabar com isso! Una-se a SoftForge para uma qualidade de ar melhor!';
+    // Títulos para cada rede social
+    let tituloTwitter = 'Compartilhar no Twitter';
+    let tituloFacebook = 'Compartilhar no Facebook';
+    let tituloReddit = 'Compartilhar no Reddit';
+    let tituloWhatsApp = 'Compartilhar no WhatsApp';
+    // URLs de compartilhamento para cada rede social
+    let urlTwitter =
+      'https://twitter.com/share?url=' + encodeURIComponent(textoCompartilhar);
+    let urlFacebook =
+      'https://www.facebook.com/sharer/sharer.php?u=' +
+      encodeURIComponent(textoCompartilhar);
+    let urlReddit =
+      'https://www.reddit.com/submit?url=' +
+      encodeURIComponent(textoCompartilhar);
+    let urlWhatsApp =
+      'https://api.whatsapp.com/send?text=' +
+      encodeURIComponent(textoCompartilhar);
+    // Largura e altura da janela pop-up
+    let larguraJanela = 500;
+    let alturaJanela = 400;
+    // Calcula a posição central da janela no navegador
+    let esquerda = (window.innerWidth - larguraJanela) / 2;
+    let topo = (window.innerHeight - alturaJanela) / 2;
+    // Abre a janela pop-up com os links de compartilhamento
+    let janelaCompartilhamento = window.open(
+      '',
+      'Compartilhar',
+      'width=' +
+        larguraJanela +
+        ', height=' +
+        alturaJanela +
+        ', top=' +
+        topo +
+        ', left=' +
+        esquerda
+    );
+    // Conteúdo HTML para a janela pop-up
+    var conteudoPopup = `
+    <style>
+    body {
+      font-family: Arial, sans-serif;
+      padding: 20px;
+    }
+    
+    h1 {
+      font-size: 20px;
+      margin-bottom: 10px;
+    }
+    
+    ul {
+      list-style: none;
+      padding: 0;
+      margin: 0;
+    }
+    
+    li {
+      margin-bottom: 10px;
+    }
+    
+    a {
+      display: block;
+      padding: 10px;
+      background-color: #f5f5f5;
+      color: #333;
+      text-decoration: none;
+      border-radius: 5px;
+    }
+    
+    a:hover {
+      background-color: #ddd;
+    }
+  </style>
+  
+  <h1>Compartilhar</h1>
+  <ul>
+    <li><a href="${urlTwitter}" target="_blank">${tituloTwitter}</a></li>
+    <li><a href="${urlFacebook}" target="_blank">${tituloFacebook}</a></li>
+    <li><a href="${urlReddit}" target="_blank">${tituloReddit}</a></li>
+    <li><a href="${urlWhatsApp}" target="_blank">${tituloWhatsApp}</a></li>
+  </ul>
+`;
+    // Define o conteúdo HTML na janela pop-up
+    janelaCompartilhamento.document.body.innerHTML = conteudoPopup;
+  }
+
+  return (
+    <>
+      <main>
+        <div>
+          <h2 id='title'>Cansado de respirar ar sujo?</h2>
+          <p className='initial-text'>
+            Asma, infecções das vias aéreas superiores e incidência de câncer de
+            pulmão e doenças cardiovasculares são apenas alguns dos sintomas que
+            respirar poluição causa ao ser humano.
+          </p>
+          <p className='initial-text'>
+            Junte-se a iniciativa capaz de acabar com isso!
+          </p>
+          <form name='increva-se'>
+            <input
+              type='text'
+              placeholder='Alexandre Carlos '
+              required='required'
+              name='name'
+              autoComplete='given-name'
+              onChange={(e) => setName(e.target.value)}
+            />
+            <InputMask
+              mask='(99)99999-9999'
+              maskChar='_'
+              placeholder='(00)00000-0000'
+              onChange={(e) => setTel(e.target.value)}
+            />
+            <input
+              type='text'
+              name='email'
+              placeholder='name@mail.com'
+              autoComplete='off'
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <textarea
+              placeholder='Deixe suas dúvidas ou comentários'
+              name='coment'
+            ></textarea>
+            <button onClick={handleClick}>Enviar</button>
+          </form>
+          <button onClick={handleShare} id='btn-compartilhar'>
+            Compartilhe nas redes sociais!
+          </button>
+        </div>
+      </main>
+    </>
+  );
+};
