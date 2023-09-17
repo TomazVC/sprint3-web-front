7fc64eb652274313f132c5c96c0c668049c0ec8a
Lilith Gouveia <lilithgouveia@outlook.com>
2023-09-17 19:56:12 -0300

    Adiionando css referente a página AquaT pelo mundo

--- src/pages/PeloMundo.jsx
+++ src/pages/PeloMundo.jsx
@@ -1,11 +1,13 @@
 import Cabecalho from "../components/Cabecalho";
 import Rodape from "../components/Rodape";
+import MainPeloMundo from "../components/MainPeloMundo";
 
 
 export default function PeloMundo(){
     return(
         <>
             <Cabecalho/>
+            <MainPeloMundo />
             <Rodape/>
         </>
     )
